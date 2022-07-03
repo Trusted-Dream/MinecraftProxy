0fbec015bb947d67dedda8d5322389774052c4bc
Trusted-Dream <trusted.dream.fun@gmail.com>
2022-07-03 14:37:42 +0900

    add test

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import pytest
+import string
+import secrets
+
+@pytest.fixture(autouse=True)
+def Tk():
+    root = tk.Tk()
+    root.title(u"Test_MinecraftProxy")
+    root.resizable(False,False)
+    root.update_idletasks()
+    w = root.winfo_width()
+    h = root.winfo_height()
+    scw = root.winfo_screenwidth()
+    sch = root.winfo_screenheight()
+    geometry = "+{:d}+{:d}".format(
+        int((scw - w) / 2),
+        int((sch - h) / 2)
+    )
+    root.geometry("450x150"+geometry)
+    root.attributes("-alpha",0.8)
+    root.iconbitmap(default="")
+    root.after(5, root.destroy)
+    return root
+
+@pytest.fixture
+def TestAPI(size=61):
+    chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    password = ''.join(secrets.choice(chars) for x in range(size))
+    return password
---
+++ tests/test_mutex.py
@@ -0,0 +1,7 @@
+import pytest
+from libs.tasktray import Tray
+
+# class Test_Mutex:
+#     def test_runnner(self):
+#         t = Tray()
+#         print (t.run())
